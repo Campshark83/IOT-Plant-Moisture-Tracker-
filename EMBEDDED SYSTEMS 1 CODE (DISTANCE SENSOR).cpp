0c659dea9f7cb3f0ee865a6e4829cd94dc3544bf
Campshark83 <149013426+Campshark83@users.noreply.github.com>
2024-06-05 17:58:24 -0400

    Create EMBEDDED SYSTEMS 1 CODE (DISTANCE SENSOR).cpp

---
+++ EMBEDDED SYSTEMS 1 CODE (DISTANCE SENSOR).cpp
@@ -0,0 +1,23 @@
+#define  MAX_RANG      (520)//the max measurement vaule of the module is 520cm(a little bit longer than  effective max range)
+#define  ADC_SOLUTION      (1023.0)//ADC accuracy of Arduino UNO is 10bit
+
+int sensityPin = A0;    // select the input pin 
+int zero = 0;
+void setup() {
+  // Serial init
+  Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+}
+float dist_t, sensity_t;
+void loop() {
+  // read the value from the sensor:
+ sensity_t = analogRead(sensityPin);
+  // turn the ledPin on
+
+ dist_t = sensity_t * MAX_RANG  / ADC_SOLUTION;//
+ Serial.print(dist_t, zero);
+ Serial.println("cm");
+
+ delay(500); 
+
+}
